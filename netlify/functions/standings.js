e4b89ae4a1445d4b3b74b4eaf01a8ae28641c7a1
casiraghiclaudio <casiraghi.claudio@gmail.com>
2026-04-25 18:53:14 +0200

    Update standings.js

--- netlify/functions/standings.js
+++ netlify/functions/standings.js
@@ -1,1 +1,26 @@
-
+export async function handler() {
+  try {
+    const res = await fetch(
+      "https://v3.football.api-sports.io/standings?league=135&season=2025",
+      {
+        headers: {
+          "x-apisports-key": "97e8c2db1fdc1f3990ae0cbcb36bfb79"
+        }
+      }
+    );
+    const data = await res.json();
+    return {
+      statusCode: 200,
+      headers: {
+        "Access-Control-Allow-Origin": "*",
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify(data)
+    };
+  } catch (e) {
+    return {
+      statusCode: 500,
+      body: JSON.stringify({ error: e.message })
+    };
+  }
+}
